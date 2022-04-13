4afe95be0740b275fdd47a258cae1d99339e3086
bellk <bellk@bellk-MacPro.local>
2022-04-13 17:56:46 +0900

    modify charging time

--- common/component.py
+++ common/component.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class base:
     def __init__(self, df):
         self.df = df
@@ -14,3 +16,37 @@ class base:
         self.df['hour'] = self.df[column].dt.hour
         self.df['minute'] = self.df[column].dt.minute
         return self.df
+
+    def chtime_by_hour(self, df):
+        ct_hour = []
+        for i in range(len(df)):
+            if df['start_time'].dt.hour[i] != df['end_time'].dt.hour[i]:
+                ct_hour.append(df.loc[i])
+
+        station_hour = pd.DataFrame(ct_hour)
+        station_hour['hour'] = station_hour['end_time'].dt.hour
+        dc_charger = pd.concat([df, station_hour]).sort_values('start_time').reset_index(drop=True)
+
+        dc_charger['st_sec'] = 3600 - (dc_charger['minute'] * 60)  # 충전 시작시간에서 가능한 충전시간
+        dc_charger['ct_sec'] = (dc_charger['end_time'] - dc_charger[
+            'start_time']).dt.total_seconds()  # 충전시간
+        dc_charger['ct_sec'] = dc_charger['ct_sec'].astype(int)
+        dc_charger['st_sec'] = dc_charger['st_sec'].astype(int)
+
+        dc_charger['ct_subtract'] = 0  # 충전시간에서 충전 가능시간을 제외한 시간
+        dc_charger['ct_next'] = 0  # 충전시간에서 충전 시작시간의 가능 시간을 제외한 시간
+        dc_charger['charging_time'] = 0  # 충전시간
+        dc_charger['ct_subtract'] = dc_charger['st_sec'] - dc_charger['ct_sec']
+
+        for i in range(len(dc_charger)):
+            if dc_charger['ct_subtract'][i] < 0:
+                dc_charger['charging_time'][i] = dc_charger['st_sec'][i]
+                dc_charger['ct_next'][i + 1] = dc_charger['ct_subtract'][i]
+            else:
+                dc_charger['charging_time'][i] = dc_charger['ct_sec'][i]
+
+        for i in range(len(dc_charger)):
+            if ((dc_charger['ct_subtract'][i] < 0) & (dc_charger['ct_next'][i] < 0)):
+                dc_charger['charging_time'][i] = abs(dc_charger['ct_next'][i])
+
+        return dc_charger
--- preprocessor/dc_charger.py
+++ preprocessor/dc_charger.py
@@ -15,9 +15,7 @@ usage_history = mt.select_usage('charger_chargerusage')
 # 충전소 선택
 charger_station = info.station_name[0]
 print(f"charger station: {charger_station}")
-cs = usage_history[usage_history['station_name'] == charger_station]
-
-InDeokWonIT_original = usage_history[usage_history["station_name"] == info.station_name[1]]
+InDeokWonIT_original = usage_history[usage_history["station_name"] == charger_station]
 IT_col = ["charging_id", "station_name", "start_time", "end_time", "member_number", "nonmember_number", "member_name", "charging_time", "charging_capacity",
            "paid_fee","charging_fee","roaming_card_entity","charging_status"]
 InDeokWonIT_original_col = InDeokWonIT_original[IT_col].reset_index(drop=True)
@@ -30,54 +28,27 @@ InDeokWonIT = mt.select_time(InDeokWonIT_original_col, 'start_time', start_date,
 component = component.base(InDeokWonIT)
 InDeokWonIT = component.time_split('start_time')
 
-ct_hour = []
-for i in range(len(InDeokWonIT)):
-    if InDeokWonIT['start_time'].dt.hour[i] != InDeokWonIT['end_time'].dt.hour[i]:
-        ct_hour.append(InDeokWonIT.loc[i])
-
-InDeokWonIT_hour = pd.DataFrame(ct_hour)
-InDeokWonIT_hour['hour'] = InDeokWonIT_hour['end_time'].dt.hour
-
-InDeokWonIT_dc_charger = pd.concat([InDeokWonIT, InDeokWonIT_hour]).sort_values('start_time').reset_index(drop=True)
-
-InDeokWonIT_dc_charger['st_sec'] = 3600 - (InDeokWonIT_dc_charger['minute'] * 60)         #60분에서 충전 시작 분시간을 차감한 초시간
-InDeokWonIT_dc_charger['ct_sec'] = (InDeokWonIT_dc_charger['end_time'] - InDeokWonIT_dc_charger['start_time']).dt.total_seconds()    #충전시간(초)
-InDeokWonIT_dc_charger['ct_sec'] = InDeokWonIT_dc_charger['ct_sec'].astype(int)
-InDeokWonIT_dc_charger['st_sec'] = InDeokWonIT_dc_charger['st_sec'].astype(int)
-
-InDeokWonIT_dc_charger['ct_subtract'] = 0                            #충전 시작시간을 제외한 남은 시간 - 충전시간
-InDeokWonIT_dc_charger['ct_next'] = 0                                #충전시간이 시작 분시간과 합하여 60분을 초과한 시간
-InDeokWonIT_dc_charger['ct_seconds'] = 0                             #충전시간
-InDeokWonIT_dc_charger['ct_subtract'] = InDeokWonIT_dc_charger['st_sec'] - InDeokWonIT_dc_charger['ct_sec']
-
-for i in range(len(InDeokWonIT_dc_charger)):
-    if InDeokWonIT_dc_charger['ct_subtract'][i] < 0:
-        InDeokWonIT_dc_charger['ct_seconds'][i] = InDeokWonIT_dc_charger['st_sec'][i]
-        InDeokWonIT_dc_charger['ct_next'][i + 1] = InDeokWonIT_dc_charger['ct_subtract'][i]
-    else:
-        InDeokWonIT_dc_charger['ct_seconds'][i] = InDeokWonIT_dc_charger['ct_sec'][i]
-
-for i in range(len(InDeokWonIT_dc_charger)):
-    if ((InDeokWonIT_dc_charger['ct_subtract'][i] < 0) & (InDeokWonIT_dc_charger['ct_next'][i] < 0)):
-        InDeokWonIT_dc_charger['ct_seconds'][i] = abs(InDeokWonIT_dc_charger['ct_next'][i])
+InDeokWonIT_dc_charger = component.chtime_by_hour(InDeokWonIT)
+# InDeokWonIT_dc_charger1 = InDeokWonIT_dc_charger[['date','charging_cnt', 'charging_amount', 'charging_time']]
 
 period = ["hour", "weekday", "day_of_week", "month"]
-sel_period = period[0]
+select_period = period[0]
 
-InDeokWonIT_group = InDeokWonIT_dc_charger.groupby(['month', sel_period]).size().reset_index(name='charging_cnt')
+InDeokWonIT_group = InDeokWonIT_dc_charger.groupby(['month', select_period]).size().reset_index(name='charging_cnt')
 
-charging_cnt = list(InDeokWonIT_dc_charger.groupby(['month', sel_period]).size().to_numpy())
-charging_amount = list(InDeokWonIT_dc_charger.groupby(['month', sel_period])['charging_capacity'].sum())
-ct_seconds = list(InDeokWonIT_dc_charger.groupby(['month', sel_period])['ct_seconds'].mean())
+charging_cnt = list(InDeokWonIT_dc_charger.groupby(['month', select_period]).size().to_numpy())
+charging_amount = list(InDeokWonIT_dc_charger.groupby(['month', select_period])['charging_capacity'].sum())
+ct_seconds = list(InDeokWonIT_dc_charger.groupby(['month', select_period])['charging_time'].mean())
 InDeokWonIT_group['charging_amount'] = charging_amount
-InDeokWonIT_group['ct_seconds'] = ct_seconds
-occupation = InDeokWonIT_group['ct_seconds'].apply(lambda x: x / (24 * 60) * 100)
+InDeokWonIT_group['charging_time'] = ct_seconds
+occupation = InDeokWonIT_group['charging_time'].apply(lambda x: x / (24 * 60) * 100)
 InDeokWonIT_group['occupation'] = occupation
 
 InDeokWonIT_group['date'] = InDeokWonIT_group[['month', 'hour']].apply(lambda x: ' '.join(x.astype(str)), axis=1)
 
 # 월별 이용률 비교
-fig = px.bar(InDeokWonIT_group, x=sel_period, y='occupation', color='month', barmode='group', title=f"Utilization Rate per hours",
+fig = px.bar(InDeokWonIT_group, x=select_period, y='occupation', color='month', barmode='group',
+             title=f"{charger_station} - 시간대별 이용률",
              color_discrete_sequence=[
                  px.colors.qualitative.Alphabet[15],
                  px.colors.qualitative.Plotly[2],
