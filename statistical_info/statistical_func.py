24b764c5330adadd7b27d2d6e191145d2a2a620d
doroshie <>
2022-06-13 10:41:04 +0900

    주중/주말 이용률그룹 회원수 수정

--- statistical_info/statistical_func.py
+++ statistical_info/statistical_func.py
@@ -14,12 +14,12 @@ class base:
         df2['charging_time'] = round(df2['charging_time'] / 60, 2)
         return df2
 
-    def num_users(self, df, *args): # 회원번호, 비회원번호 count하여 이용자 수 계산
-        df1_group = df.groupby(['station_name', 'charger_code', 'month', 'member_number', *args]).size().reset_index(name='cnt')
-        df1 = df1_group.groupby(['month', *args])['member_number'].count().reset_index(name='mem_cnt')
-        df2_group = df.groupby(['station_name', 'charger_code', 'month', 'nonmember_number', *args]).size().reset_index(name='cnt')
-        df2 = df2_group.groupby(['month', *args])['nonmember_number'].count().reset_index(name='nonmem_cnt')
-        df3 = pd.merge(df1, df2, on=['month', 'wd_rank' ], how='inner')
+    def num_users(self, df, col): # 회원번호, 비회원번호 count하여 이용자 수 계산
+        df1_group = df.groupby(['station_name', 'charger_code', 'month', 'member_number', col]).size().reset_index(name='cnt')
+        df1 = df1_group.groupby(['month', col])['member_number'].count().reset_index(name='mem_cnt')
+        df2_group = df.groupby(['station_name', 'charger_code', 'month', 'nonmember_number', col]).size().reset_index(name='cnt')
+        df2 = df2_group.groupby(['month', col])['nonmember_number'].count().reset_index(name='nonmem_cnt')
+        df3 = pd.merge(df1, df2, on=['month', col ], how='inner')
         df3['user_cnt'] = df3['mem_cnt'] + df3['nonmem_cnt']
         return df3
 
